ee7eeffeebec591f58bd60b0526c6023d17127bb
JagrutiMahajan04 <jagrutimaha2000@gmail.com>
2022-10-29 19:21:19 +0530

    practice js

---
+++ foreachloop.js
@@ -0,0 +1,12 @@
+const students = ["arrow","function","Normal","Function"]
+
+ students.forEach((item,index)=> {
+
+    console.log(item, index)
+ }
+ ) 
+
+ function normalfunction(student,index){
+    console.log("normal function" +student)
+ }
+
